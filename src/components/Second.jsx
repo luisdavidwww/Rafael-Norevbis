e747c36454c374573fc64dbe6b2b6ff97d47b758
LuisDavid <luisdavid25torrealba@gmail.com>
2025-08-20 16:49:57 -0400

    Actualizacion de errores

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -12,6 +12,10 @@ const Second = () => {
        <div className='containerbtn-ulti'>
           <a className='btn-outline-ulti' type='button' href='https://drive.google.com/file/d/1seJJ9R4321Z6rwb6mPhEcH66de-IyoFk/view?usp=sharing'> DATOS AQUI </a>
       </div>
+
+      <div className='contenedor__ig'>
+        <p className='contenedor__ig_p'>Diseñado por <a href="https://www.instagram.com/louisdetalles/" style={{textDecoration:'none', color:'white', fontWeight:'700'}}>@louisdetalles </a></p>
+      </div>
     </div>
   )
 }
